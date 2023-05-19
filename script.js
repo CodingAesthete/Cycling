d9387d963a725705b522176b96a19873ccf5cba3
PostPunkAesthete <harry_ollI@abv.bg>
2023-05-20 02:11:20 +0300

    Fix selected class after resizing

--- script.js
+++ script.js
@@ -7,34 +7,42 @@ $(document).ready(function() {
 
     $('html, body').stop().animate({
       scrollTop: sectionOffset
-    }, 800, 'easeOutCirc',);
+    }, 800, 'easeOutCirc');
   });
 
-  $(window).scroll(function() {
-    selectSection();
-  });
-
+  var resizeTimeout;
   $(window).on('resize', function() {
-    clearTimeout(doneResizing);
+    clearTimeout(resizeTimeout);
 
-    doneResizing = setTimeout(function() {
+    resizeTimeout = setTimeout(function() {
       selectSection();
     }, 500);
+  });
+
+  $(window).on('load scroll', function() {
+    selectSection();
   });
 
   selectSection();
 
   function selectSection() {
-    const pageTop = $(window).scrollTop() + 240;
+    var pageTop = $(window).scrollTop() + 240;
+    var selectedSection = null;
 
     $('section').each(function(index) {
-      const sectionTop = $(this).offset().top;
-      const sectionBottom = sectionTop + $(this).outerHeight();
+      var sectionTop = $(this).offset().top;
+      var sectionBottom = sectionTop + $(this).outerHeight();
 
       if (pageTop >= sectionTop && pageTop < sectionBottom) {
-        $('nav ul li a').removeClass('selected');
-        $('nav ul li a').eq(index).addClass('selected');
+        selectedSection = $(this);
+        return false;
       }
     });
+
+    if (selectedSection) {
+      var selectedLinkId = selectedSection.attr('id');
+      $('nav ul li a').removeClass('selected');
+      $('nav ul li a[href="#' + selectedLinkId + '"]').addClass('selected');
+    }
   }
 });
